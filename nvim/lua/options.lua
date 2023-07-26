db7b0d6a98aa75c7687f54a30da11b3328ef0503
Blank0211 <wkgchoh@gmail.com>
2023-07-26 12:23:07 -0700

    Set splitright to true (open vertical splits to right)

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,9 +1,11 @@
 local o = vim.opt
+
+-- Appearance
 o.number = true
 o.relativenumber = true
-o.ignorecase = true
 o.termguicolors = true
 o.colorcolumn = '80'
+
 o.encoding = 'utf-8'
 o.fileencoding = 'utf-8'
 
@@ -12,4 +14,7 @@ o.expandtab = true
 o.tabstop = 2
 o.shiftwidth = 2
 o.softtabstop = 2
+
+o.ignorecase = true
+o.splitright = true
 
