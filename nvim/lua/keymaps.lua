99f19f25240c3532d17066e0128081fb8b1772a0
Blank0211 <wkgchoh@gmail.com>
2023-07-29 09:58:03 -0700

    Added desc(ription) key to keymaps & removed comments Minor changes to code

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,22 +2,25 @@ local opts = {noremap = true}
 vim.g.mapleader = ' '
 local set = vim.keymap.set
 
--- Delete word with CTRL+Backspace
-set("i", "<C-H>", "<C-W>")
--- Exit terminal mode with Esc
-set("t", "<Esc>", "<C-\\><C-n>")
--- Open Netrw
-set('n', '<leader>f', ':Ex<CR>')
--- Save & load folds
-set('n', '<leader>sf', ':mkview')
-set('n', '<leader>lf', ':loadview')
+-- Folds
+set('n', '<leader>sf', ':mkview',
+  {desc = 'Save current folds'})
+set('n', '<leader>lf', ':loadview',
+  {desc = 'Load saved folds'})
 
--- Open powershell vertically
-set('n', '<leader>ps', '<cmd>vs term://pwsh<CR>')
--- Exit powershell
-set('t', 'qq', '<cmd>quit<CR>')
+-- Powershell
+set('n', '<leader>ps', '<cmd>vs term://pwsh<CR>',
+  {desc = 'Open PS in verctical split'})
+set('t', 'qq', '<cmd>quit<CR>',
+  {desc = 'Exit PS (TERMINAL MODE ONLY)'})
+set("t", "<Esc>", "<C-\\><C-n>",
+  {desc = 'Exit Terminal mode with ESC'})
 
-
+-- Misc
+set("i", "<C-H>", "<C-W>",
+  {desc = 'Delete word with CTRL+BS'})
+set('n', '<leader>f', ':Ex<CR>',
+  {desc = 'Open file explorer (Netrw)'})
 
 
 
