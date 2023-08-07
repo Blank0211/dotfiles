32ba2cd9cd32240b9246aff4bbaaf53492ea27e1
Blank0211 <wkgchoh@gmail.com>
2023-08-06 18:35:55 -0700

    Set silent=true (keymaps will not be echoed to cmd line) Format code

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,23 +3,23 @@ local set = vim.keymap.set
 
 -- Folds
 set('n', '<leader>sf', ':mkview<CR>',
-  {desc = 'Save current folds'})
+  {desc='Save current folds', silent=true})
 set('n', '<leader>lf', ':loadview<CR>',
-  {desc = 'Load saved folds'})
+  {desc = 'Load saved folds', silent=true})
 
 -- Powershell
 set('n', '<leader>ps', '<cmd>vs term://pwsh<CR>',
-  {desc = 'Open PS in verctical split'})
+  {desc='Open PS in verctical split'})
 set('t', 'qq', '<cmd>quit<CR>',
-  {desc = 'Exit PS (TERMINAL MODE ONLY)'})
+  {desc='Exit PS (TERMINAL MODE ONLY)'})
 set("t", "<Esc>", "<C-\\><C-n>",
-  {desc = 'Exit Terminal mode with ESC'})
+  {desc='Exit Terminal mode with ESC'})
 
 -- Misc
 set("i", "<C-H>", "<C-W>", -- NOTE: <C-BS> does not work on windows.
-  {desc = 'Delete word with CTRL+BS'})
+  {desc='Delete word with CTRL+BS'})
 set('n', '<leader>f', ':Ex<CR>',
-  {desc = 'Open file explorer (Netrw)'})
+  {desc='Open file explorer (Netrw)'})
 
 
 
