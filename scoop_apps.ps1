5b4ea597828e2a285d1cfdcc5e59e96e66a23569
Blank0211 <wkgchoh@gmail.com>
2023-08-14 18:41:11 -0700

    Setup for buckets and utils

--- scoop_apps.ps1
+++ scoop_apps.ps1
@@ -3,3 +3,14 @@
   Description: Powershell script to install programs using scoop
 #>
 
+# Necessary buckets
+scoop bucket add extras
+scoop bucket add versions
+scoop bucket add sysinternals
+
+# Scoop can utilize aria2 for multi-connection downloads
+scoop install aria2
+
+# Utils
+scoop install sumatrapdf mpc-be mpv xnview everything
+
