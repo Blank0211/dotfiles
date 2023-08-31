9de47921d1647ee46714677ab97d6e2e3d720cb4
Blank0211 <wkgchoh@gmail.com>
2023-08-31 22:28:07 +0530

    Add mini.clue plugin
    Copied quickstart setup from github page, which is necessary
    for plugin to work

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,9 +3,10 @@
 -- =========================
 require('paq') {
   'savq/paq-nvim', -- Let Paq manage itself
-  
+
   'm4xshen/autoclose.nvim', -- Autoclose
   'folke/tokyonight.nvim', -- Colorscheme
+  'echasnovski/mini.clue' -- Mini.Clue
 }
 
 -- ===================
@@ -25,4 +26,49 @@ require('tokyonight').setup {
 }
 vim.cmd[[colorscheme tokyonight]]
 
+-- [ Mini.Clue ]
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    {mode = 'n', keys = '<Leader>'},
+    {mode = 'x', keys = '<Leader>'},
 
+    -- Built-in completion
+    {mode = 'i', keys = '<C-x>'},
+
+    -- `g` key
+    {mode = 'n', keys = 'g'},
+    {mode = 'x', keys = 'g'},
+
+    -- Marks
+    {mode = 'n', keys = "'"},
+    {mode = 'n', keys = '`'},
+    {mode = 'x', keys = "'"},
+    {mode = 'x', keys = '`'},
+
+    -- Registers
+    {mode = 'n', keys = '"'},
+    {mode = 'x', keys = '"'},
+    {mode = 'i', keys = '<C-r>'},
+    {mode = 'c', keys = '<C-r>'},
+
+    -- Window commands
+    {mode = 'n', keys = '<C-w>'},
+
+    -- `z` key
+    {mode = 'n', keys = 'z'},
+    {mode = 'x', keys = 'z'},
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+})
+ 
