91cbbd46725bee0de213061a29b92f78081dd7d7
Blank0211 <wkgchoh@gmail.com>
2023-09-01 08:42:13 +0530

    Add keymap for MiniFiles.open

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -12,12 +12,13 @@ set('n', '<leader>ps', '<cmd>vs term://pwsh<CR>',
   {desc = 'Open PS in verctical split'})
 set('t', 'qq', '<cmd>quit<CR>',
   {desc = 'Exit PS (TERMINAL MODE ONLY)'})
-set("t", "<Esc>", "<C-\\><C-n>",
+set('t', '<Esc>', '<C-\\><C-n>',
   {desc = 'Exit Terminal mode with ESC'})
 
 -- Misc
-set("i", "<C-H>", "<C-W>", -- NOTE: <C-BS> does not work on windows.
+set('i', '<C-H>', '<C-W>', -- NOTE: <C-BS> does not work on windows.
   {desc = 'Delete word with CTRL+BS'})
-
+set('i', '<leader>ff', MiniFiles.open,
+  {desc = 'Open MiniFiles Explorer'})
 
 
