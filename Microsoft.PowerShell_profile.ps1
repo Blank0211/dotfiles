95b82a4281746adb5cc5d37ebc3b2cd3f3d30baf
Blank0211 <wkgchoh@gmail.com>
2023-09-09 22:42:26 +0530

    Check if compiler is gcc or clang.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,15 +3,18 @@ $myvar = "myvar haha"
 Import-Module scoop-completion
 
 
+# compile c files.
 function ccp {
     param ([string]$Compiler = "clang",
            [string]$InputFile = "waterfall.c",
            [string]$OutputFile = "a.exe")
 
+    # check if $compiler is clang or gcc, if not return.
     if (($Compiler -cne "clang") -and ($Compiler -cne "gcc")) {
         Write-Output "Invalid value for -compiler param: $Compiler"
         return
     }
+
     $flags = "-std=c18 -Wall -Wextra -pedantic -Wformat=2"
     $cmd = "$Compiler $InputFile -o $OutputFile $flags"
     Invoke-Expression $cmd
