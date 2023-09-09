7f96d9f66ee3053a2d7ac2d9a95c2005be8532bb
Blank0211 <wkgchoh@gmail.com>
2023-09-09 22:03:08 +0530

    Add function to compile c files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
 $PSStyle.FileInfo.Directory = ""
 $myvar = "myvar haha"
 Import-Module scoop-completion
+
+
+function ccp([string]$compiler = "clang") {
+    $cmd = "$compiler waterfall.c -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2"
+    Invoke-Expression $cmd
+}
+
