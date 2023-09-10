d60c3ff896d2e47a69ae16b3b3b27185e4159d59
Blank0211 <wkgchoh@gmail.com>
2023-09-10 21:46:38 +0530

    Remove powershell keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,14 +7,6 @@ set('n', '<leader>sf', ':mkview<CR>',
 set('n', '<leader>lf', ':loadview<CR>',
   {desc = 'Load saved folds', silent=true})
 
--- Powershell
-set('n', '<leader>ps', '<cmd>vs term://pwsh<CR>',
-  {desc = 'Open PS in verctical split'})
-set('t', 'qq', '<cmd>quit<CR>',
-  {desc = 'Exit PS (TERMINAL MODE ONLY)'})
-set('t', '<Esc>', '<C-\\><C-n>',
-  {desc = 'Exit Terminal mode with ESC'})
-
 -- Misc
 set('i', '<C-H>', '<C-W>', -- NOTE: <C-BS> does not work on windows.
   {desc = 'Delete word with CTRL+BS'})
