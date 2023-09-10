4f7d7fa47e9c09bf3b69d32ee81dd38b7a594a5b
Blank0211 <wkgchoh@gmail.com>
2023-09-10 21:59:09 +0530

    Add keymaps for compiling current C file

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -13,4 +13,11 @@ set('i', '<C-H>', '<C-W>', -- NOTE: <C-BS> does not work on windows.
 set('n', '<leader>ff', MiniFiles.open,
   {desc = 'Open MiniFiles Explorer'})
 
+-- Compile C files
+local gcc = ":!gcc % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2<CR>"
+local clang = ":!clang % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2<CR>"
+set('n', '<leader>cg', gcc,
+  {desc = 'Compile current C file with gcc'})
+set('n', '<leader>cc', clang,
+  {desc = 'Compile current C file with clang'})
 
