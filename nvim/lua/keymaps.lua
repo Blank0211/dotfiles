aabb01808be0b4d49d8d3d95b1828ea73716908e
Blank0211 <wkgchoh@gmail.com>
2023-09-26 22:07:30 +0530

    Require 'keymaps' before 'plugins'
    Fix: requiring 'keymaps' before 'plugins' will not throw error.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,8 +8,9 @@ set('n', '<leader>lf', ':loadview<CR>',
   {desc = 'Load saved folds'})
 
 -- Misc
-set('i', '<C-H>', '<C-W>', -- NOTE: <C-BS> does not work on windows.
+set('i', '<C-H>', '<C-W>', -- NOTE: <C-BS> does not work on Windows.
   {desc = 'Delete word with CTRL+BS'})
+MiniFiles = require('mini.files')
 set('n', '<leader>ff', MiniFiles.open,
   {desc = 'Open MiniFiles Explorer'})
 
