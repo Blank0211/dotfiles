b55909fdb813f852d1ff77b4e8e33d655113b203
Blank0211 <wkgchoh@gmail.com>
2023-09-27 22:38:32 +0530

    Add comment-based help for ccp

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,22 @@ Set-Alias -Name vim -Value nvim
 
 # compile c files.
 function ccp {
+    <#
+    .SYNOPSIS
+    Compile C file with gcc or clang
+    .Description
+    Compiles a c file named "waterfall.c" with clang and outputs "a.exe".
+    This is the default behaviour, which can be changed by passing
+    relevant parameters.
+    .Parameter Compiler
+    Program to use for compiling.
+    Default value: clang. any value other than gcc or clang is invalid.
+    .Parameter InputFile
+    The file to compile.
+    Default value: "waterfall.c". 
+    .Parameter OutputFile
+    The name of output executable file
+    #>
     param ([string]$Compiler = "clang",
            [string]$InputFile = "waterfall.c",
            [string]$OutputFile = "a.exe")
