2116a7a98c517b820489cd8a7a46394b81598adb
Blank0211 <wkgchoh@gmail.com>
2023-10-03 22:22:58 +0530

    Minor change

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@ local modules = {
 for _, module in ipairs(modules) do
     local ok, err = pcall(require, module)
     if not ok then
-        error("Error loading " .. module .. "\n\n" .. err)
+        error("Error loading " .. module .. " module\n\n" .. err)
     end
 end
     
