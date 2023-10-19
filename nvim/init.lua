1ea56976cfd79911813da894fc43b0e9e77dbf0c
Blank0211 <wkgchoh@gmail.com>
2023-10-19 22:36:29 +0530

    Replace `error()` with `print()`

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,7 @@ local modules = {
 for _, module in ipairs(modules) do
     local ok, err = pcall(require, module)
     if not ok then
-        error("Error loading " .. module .. " module\n\n" .. err)
+        print("Error loading " .. module .. " module\n\n" .. err)
     end
 end
 
