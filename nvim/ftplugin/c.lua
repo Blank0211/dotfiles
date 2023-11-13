19de38ffa5f25411f69933aaa7f64d1747c4302f
Blank0211 <wkgchoh@gmail.com>
2023-11-13 23:06:19 +0530

    Add mapping for comment toggle
    mapping is buffer local

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -1,4 +1,16 @@
+-- vim.g.maplocalleader = ','
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
+
+function cmnt()
+    -- Get line & strip leading white space
+    local line = vim.api.nvim_get_current_line():match('^%s*(.*)')
+    line = line:sub(1, 2)
+
+    return line == '//' and '^dw$' or 'I// <ESC>$'
+end
+
+vim.keymap.set('n', '<Leader>mm', cmnt,
+    {desc = 'Toggle comment on current line', expr = true, buffer = 0})
 
