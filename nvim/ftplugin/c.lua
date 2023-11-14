4c070f0f0dfe544d7b2937ced11e6509fb4492f9
Blank0211 <wkgchoh@gmail.com>
2023-11-14 22:01:40 +0530

    Remove unnecessary assignment

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -5,9 +5,8 @@ vim.opt_local.softtabstop = 4
 function cmnt()
     -- Get line & strip leading white space
     local line = vim.api.nvim_get_current_line():match('^%s*(.*)')
-    line = line:sub(1, 2)
-
-    return line == '//' and '^dw$' or 'I// <ESC>$'
+    -- if first to chars == '//' ...
+    return line:sub(1, 2) == '//' and '^dw$' or 'I// <ESC>$'
 end
 
 vim.keymap.set('n', '<Leader>mm', cmnt,
