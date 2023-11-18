aae088251150c2cd6e71b6ee891d1f638c36ad73
Blank0211 <wkgchoh@gmail.com>
2023-11-18 22:31:20 +0530

    Remove unnecessary variable

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -18,10 +18,9 @@ vim.keymap.set('n', '<Leader>mm', cmnt,
     {desc = 'Toggle comment on current line', expr = true, buffer = 0})
 
 -- Compile C files
-local gcc = ":!gcc % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2 -g<CR>"
-local clang = ":!clang % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2 -g<CR>"
-vim.keymap.set('n', '<leader>cg', gcc,
+local c_cmd = " % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2 -g<CR>"
+vim.keymap.set('n', '<leader>cg', ":!gcc" .. c_cmd,
     {desc = 'Compile current C file with gcc', buffer = 0})
-vim.keymap.set('n', '<leader>cc', clang,
+vim.keymap.set('n', '<leader>cc', ":!clang" .. c_cmd,
     {desc = 'Compile current C file with clang', buffer = 0})
 
