f2e7ddc7f4ebaf22990b9b1c973de74de7345e89
Blank0211 <wkgchoh@gmail.com>
2023-11-18 22:04:53 +0530

    Move keymaps for compiling C file:
        lua/keymaps.lua > ftplugin/c.lua

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -8,12 +8,20 @@ vim.opt_local.softtabstop = 4
 -- =============
 -- [[ KEYMAPS ]] 
 -- =============
--- Return '^dw$' or 'I// <ESC>$', depending on the first two chars of the line
 function cmnt()
     -- get line & strip leading whitespace
     local line = vim.api.nvim_get_current_line():match('^%s*(.*)')
+    -- Return '^dw$' or 'I// <ESC>$', depending on the first two chars of the line
     return line:sub(1, 2) == '//' and '^dw$' or 'I// <ESC>$'
 end
 vim.keymap.set('n', '<Leader>mm', cmnt,
     {desc = 'Toggle comment on current line', expr = true, buffer = 0})
+
+-- Compile C files
+local gcc = ":!gcc % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2<CR>"
+local clang = ":!clang % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2<CR>"
+vim.keymap.set('n', '<leader>cg', gcc,
+    {desc = 'Compile current C file with gcc', buffer = 0})
+vim.keymap.set('n', '<leader>cc', clang,
+    {desc = 'Compile current C file with clang', buffer = 0})
 
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -14,11 +14,3 @@ MiniFiles = require('mini.files')
 set('n', '<leader>ff', MiniFiles.open,
   {desc = 'Open MiniFiles Explorer'})
 
--- Compile C files
-local gcc = ":!gcc % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2<CR>"
-local clang = ":!clang % -o a.exe -std=c18 -Wall -Wextra -pedantic -Wformat=2<CR>"
-set('n', '<leader>cg', gcc,
-  {desc = 'Compile current C file with gcc'})
-set('n', '<leader>cc', clang,
-  {desc = 'Compile current C file with clang'})
-
