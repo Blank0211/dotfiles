83ccb11e590372e71e477a781d019363ff8b40f4
Blank0211 <wkgchoh@gmail.com>
2023-12-05 17:42:26 +0530

    Fix mistake for font setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,7 +11,7 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-wezterm.font("JetBrainsMono NFM", {weight="Regular", stretch="Normal", style="Normal"})
+config.font = wezterm.font("JetBrainsMono NFM", {weight="Regular", stretch="Normal", style="Normal"})
 config.color_scheme = 'tokyonight_night'
 config.default_prog = {'pwsh.exe', '-nologo'}
 config.hide_tab_bar_if_only_one_tab = true
@@ -26,3 +26,4 @@ config.keys = {
 
 -- and finally, return the configuration to wezterm
 return config
+
